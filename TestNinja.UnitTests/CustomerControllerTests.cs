cad819f7b32a39ebd46d16348c9d3c8cc76accdf
Elena Siso Rodríguez <ESO@nextret.net>
2018-11-12 17:04:06 +0100

    Add project files.

---
+++ TestNinja.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            var controller = new CustomerController();
+
+            var result = controller.GetCustomer(0);
+            
+            // NotFound 
+            Assert.That(result, Is.TypeOf<NotFound>());
+            
+            // NotFound or one of its derivatives 
+//            Assert.That(result, Is.InstanceOf<NotFound>());
+        }
+
+        [Test]
+        public void GetCustomer_IdIsNotZero_ReturnOk()
+        {
+            //(esiso) Todo lo que está implementado en este método lo he añadido yo.
+            var controller = new CustomerController();
+
+            var result = controller.GetCustomer(1);
+
+            Assert.That(result, !Is.TypeOf<NotFound>());
+            
+        }
+    }
+}
